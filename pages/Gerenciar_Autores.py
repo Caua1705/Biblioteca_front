7e0114508602c9e8715c3df3b2989ad4ab786574
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-11-09 23:24:14 -0300

    d

--- pages/Gerenciar_Autores.py
+++ pages/Gerenciar_Autores.py
@@ -48,6 +48,9 @@ with abas[2]:
         autor_nome = st.selectbox("Selecione o autor para editar", list(lista_autores.keys()))
         autor_selecionado = lista_autores[autor_nome]
 
+        st.session_state["autor_selecionado"] = autor_selecionado
+        autor_selecionado = st.session_state["autor_selecionado"]
+
         nome = st.text_input("Nome do Autor", autor_selecionado["nomeAutor"], key="edit_nome_autor")
         data_nascimento = st.date_input("Data de Nascimento", pd.to_datetime(autor_selecionado["dataNascimento"]), format="DD/MM/YYYY", key="edit_data_nasc")
         nacionalidade = st.text_input("Nacionalidade", autor_selecionado["nacionalidade"], key="edit_nacionalidade")
